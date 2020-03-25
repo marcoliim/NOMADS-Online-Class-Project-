92c3b92a6dd5fff2265d1ef1d1f6a58835d27360
marcoliim <maarrcolim@gmail.com>
2020-03-25 17:01:36 +0700

    gallery change

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -65,9 +65,9 @@
         @foreach ($items as $item)
         <div class="col-sm-6 col-md-4 col-lg-3">
           <div
-            class="card-travel text-center d-flex flex-column"
-            style="background-image: url('{{ $item->galleries->count() ? Storage::url
-            ($item->galleries->first()->images) : '' }}');"
+                class="card-travel text-center d-flex flex-column"
+                style="background-image: url('{{ $item->galleries->count() ? Storage::url
+                ($item->galleries->first()->image) : ''  }}');"
           >
             <div class="travel-country">{{ $item->location }}</div>
             <div class="travel-location">{{ $item->title }}</div>
